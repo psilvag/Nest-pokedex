8351a59b66f0a04416c33c5820c963715054bc37
Pablo Silva <pmarcelosilvag@gmail.com>
2023-11-07 11:56:31 -0400

    Antes de insertar  la data a la DB por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 
@@ -17,7 +18,8 @@ import { CommonModule } from './common/common.module';
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     //Importamos los modulos
     PokemonModule,
-    CommonModule
+    CommonModule,
+    SeedModule
 
   ]
  
